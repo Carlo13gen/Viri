9d37c98553ded2f40c4ee06ec3f68eb88fb58cd6
chiello <carloiurat@virgilio.it>
2019-08-26 11:01:40 +0200

    All .nex files uploaded on database
    TODO: try to collect all the data inside the file table and then show them in a list in the new homepage

--- project2.py
+++ project2.py
@@ -118,7 +118,7 @@ def upload_file():
 				output_filename = secure_filename(new_out_name)
 				input.save(os.path.join(app.config["INPUT_UPLOAD"], input_filename))
 				output.save(os.path.join(app.config["OUTPUT_UPLOAD"], output_filename))
-				if perhand.save_files_path(input_filename, output_filename, os.path.abspath(os.path.join(app.config["INPUT_UPLOAD"], input_filename)), os.path.abspath(os.path.join(app.config["OUTPUT_UPLOAD"], output_filename)), flask_login.current_user.id, description):
+				if perhand.save_files_path(input_filename, output_filename, os.path.relpath(os.path.join(app.config["INPUT_UPLOAD"], input_filename)), os.path.relpath(os.path.join(app.config["OUTPUT_UPLOAD"], output_filename)), flask_login.current_user.id, description):
 					return redirect(request.url)
 				else:
 					x = 'Something went wrong during the saving on db'
