fe6124aa221421c8dd8c7bd280110ec8bc77a123
chiello <carloiurat@virgilio.it>
2019-09-10 18:19:11 +0200

    Now it all works

--- project2.py
+++ project2.py
@@ -115,10 +115,10 @@ def upload_file():
 				new_out_name = name_partition_out[0] + "_" + str(flask_login.current_user.id) + "." + name_partition_out[2]
 				input_filename = secure_filename(new_nex_name)
 				output_filename = secure_filename(new_out_name)
-				input.save(os.path.join(app.config["INPUT_UPLOAD"], input_filename))
-				output.save(os.path.join(app.config["OUTPUT_UPLOAD"], output_filename))
-				if name_partition_nex[0]==name_partition_out[0]:
+				if name_partition_nex[0] == name_partition_out[0]:
 					if perhand.save_files_path(input_filename, output_filename, os.path.relpath(os.path.join(app.config["INPUT_UPLOAD"], input_filename)), os.path.relpath(os.path.join(app.config["OUTPUT_UPLOAD"], output_filename)), flask_login.current_user.id, description):
+						input.save(os.path.join(app.config["INPUT_UPLOAD"], input_filename))
+						output.save(os.path.join(app.config["OUTPUT_UPLOAD"], output_filename))
 						return redirect(request.url)
 					else:
 						x = 'Something went wrong during the saving on db'
