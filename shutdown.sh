f2c75371548fcb11fe34465f6e7b48ce545e605b
Maurizio Patrignani <maurizio.patrignani@uniroma3.it>
2019-11-07 12:07:34 +0100

    ora shutdown.sh funziona quale che sia l'xterm utilizzato dall'utente

--- shutdown.sh
+++ shutdown.sh
@@ -1,8 +1,16 @@
 #/bin/bash
 
 echo "killing:"
-ps aux | grep gnome-terminal
+ps aux | grep "\-e" | grep portaGDTEucalipt
+ps aux | grep "\-e" | grep portaGDTEucalipt | gawk -F" " '{print  $2}' | xargs kill -9
 
-ps aux | grep gnome-terminal | gawk -F" " '{print  $2}' | xargs kill -9
-z
+echo "killing:"
+ps aux | grep "\-e" | grep portaInizializzazioneESalvataggio
+ps aux | grep "\-e" | grep portaInizializzazioneESalvataggio | gawk -F" " '{print  $2}' | xargs kill -9
+
+echo "killing:"
+ps aux | grep "\-e" | grep project2
+ps aux | grep "\-e" | grep project2 | gawk -F" " '{print  $2}' | xargs kill -9
+
+
 
